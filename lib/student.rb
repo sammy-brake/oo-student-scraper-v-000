87cb521b6cacd9ff582bc267cea9c683acca75d5
Samantha brake <sammy.brake@gmail.com>
2018-10-26 17:28:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       Student.new(student)
-    end 
+    end
   end
 
   def add_student_attributes(attributes_hash)
