460d8f9499e488ab62153b44bae804f333c316c8
Samantha brake <sammy.brake@gmail.com>
2018-10-26 17:30:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,9 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    end 
+    self 
   end
 
   def self.all
